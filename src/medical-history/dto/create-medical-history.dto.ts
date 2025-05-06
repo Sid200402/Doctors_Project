50e2cd90647130b800ec5e9561a08fa72d4fb50d
Siddhartha Majumder <siddhartha191122@gmail.com>
2025-05-06 19:14:12 +0530

    Patient service modified

--- src/medical-history/dto/create-medical-history.dto.ts
+++ src/medical-history/dto/create-medical-history.dto.ts
@@ -1,1 +1,19 @@
-export class CreateMedicalHistoryDto {}
+import { IsEnum, IsOptional, IsString, IsUUID, IsDateString } from 'class-validator';
+import { MedicalHistoryType } from '../../enum';
+
+export class CreateMedicalHistoryDto {
+  @IsUUID()
+  @IsOptional()
+  patientId?: string;
+
+  @IsEnum(MedicalHistoryType)
+  type: MedicalHistoryType;
+
+  @IsOptional()
+  @IsString()
+  desc?: string;
+
+  @IsOptional()
+  @IsDateString()
+  occurredOn?: string;
+}
