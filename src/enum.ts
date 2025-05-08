0e95a69b207fb3b0caf41a3a5f0afba4cc08b3de
Siddhartha Majumder <siddhartha191122@gmail.com>
2025-05-08 17:16:58 +0530

    Prescription Module complete

--- src/enum.ts
+++ src/enum.ts
@@ -1,67 +1,81 @@
 export enum UserRole {
 
-    DOCTOR = 'DOCTOR',
-    ADMIN = 'ADMIN',
-    PATIENT = 'PATIENT',
-  }
-  export enum DefaultStatus {
-    ACTIVE = 'ACTIVE',
-    DEACTIVE = 'DEACTIVE',
-    DELETED = 'DELETED',
-    SUSPENDED = 'SUSPENDED',
-    PENDING = 'PENDING',
-  }
-  export enum PageType {
-    TNC = 'TERMS & CONDITIONS',
-    PRIVACY_POLICY = 'PRIVACY POLICY',
-    DATA_POLICY = 'DATA POLICY',
-  }
-  export enum Gender{
-    MALE = 'MALE',
-    FEMALE="FEMALE",
-    OTHER="OTHER"
-  }
+  DOCTOR = 'DOCTOR',
+  ADMIN = 'ADMIN',
+  PATIENT = 'PATIENT',
+}
+export enum DefaultStatus {
+  ACTIVE = 'ACTIVE',
+  DEACTIVE = 'DEACTIVE',
+  DELETED = 'DELETED',
+  SUSPENDED = 'SUSPENDED',
+  PENDING = 'PENDING',
+}
+export enum PageType {
+  TNC = 'TERMS & CONDITIONS',
+  PRIVACY_POLICY = 'PRIVACY POLICY',
+  DATA_POLICY = 'DATA POLICY',
+}
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = "FEMALE",
+  OTHER = "OTHER"
+}
 
-  export enum AIType {
-    ACTIVE = 'ACTIVE',
-    INACTIVE = 'INACTIVE',
-  }
-  export enum BloodGroup {
-    A_POSITIVE = 'A+',
-    A_NEGATIVE = 'A-',
-    B_POSITIVE = 'B+',
-    B_NEGATIVE = 'B-',
-    AB_POSITIVE = 'AB+',
-    AB_NEGATIVE = 'AB-',
-    O_POSITIVE = 'O+',
-    O_NEGATIVE = 'O-',
-    UNKNOWN = 'Unknown'
-  }
+export enum AIType {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+}
 
-  export enum MaritalStatus {
-    SINGLE = 'Single',
-    MARRIED = 'Married',
-    DIVORCED = 'Divorced',
-    WIDOWED = 'Widowed',
-    OTHER = 'Other'
-  }
-  export enum CaseStatus {
-    NEW = 'NEW',
-    IN_CONSULTATION = 'IN_CONSULTATION',
-    TREATED = 'TREATED',
-    CLOSED = 'CLOSED',
-  }
 
-  export enum AppointmentStatus {
-    SCHEDULED = 'SCHEDULED',
-    CHECKED_IN = 'CHECKED_IN',
-    IN_PROGRESS = 'IN_PROGRESS',
-    COMPLETED = 'COMPLETED',
-    CANCELLED = 'CANCELLED',
-    NO_SHOW = 'NO_SHOW',
-  }
 
-  export enum AppointmentType {
-    WALK_IN = 'WALK_IN',
-    SCHEDULED = 'SCHEDULED',
-  }
+
+
+
+
+
+
+
+
+
+
+export enum BloodGroup {
+  A_POSITIVE = 'A+',
+  A_NEGATIVE = 'A-',
+  B_POSITIVE = 'B+',
+  B_NEGATIVE = 'B-',
+  AB_POSITIVE = 'AB+',
+  AB_NEGATIVE = 'AB-',
+  O_POSITIVE = 'O+',
+  O_NEGATIVE = 'O-',
+  UNKNOWN = 'Unknown'
+}
+
+export enum MaritalStatus {
+  SINGLE = 'Single',
+  MARRIED = 'Married',
+  DIVORCED = 'Divorced',
+  WIDOWED = 'Widowed',
+  OTHER = 'Other'
+}
+export enum CaseStatus {
+  NEW = 'NEW',
+  IN_CONSULTATION = 'IN_CONSULTATION',
+  TREATED = 'TREATED',
+  CLOSED = 'CLOSED',
+}
+
+export enum AppointmentStatus {
+  SCHEDULED = 'SCHEDULED',
+  CHECKED_IN = 'CHECKED_IN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED',
+  NO_SHOW = 'NO_SHOW',
+}
+
+export enum AppointmentType {
+  WALK_IN = 'WALK_IN',
+  SCHEDULED = 'SCHEDULED',
+}
+
